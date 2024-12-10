15ac9e4f4fcce7c1743a54a4d89b7dacdc72a417
Vinayak Pandey <vinayakpandey@KTPLs-MacBook-Air.local>
2024-12-10 18:35:15 +0530

    changed image

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ["www.shutterstock.com","source.unsplash.com","images.unsplash.com","www.floridaleathergallery.com","dlifeinteriors.com","weadesign.com","plus.unsplash.com","st.hzcdn.com"],
+        domains: ["ddesigndoor.com  ","www.shutterstock.com","source.unsplash.com","images.unsplash.com","www.floridaleathergallery.com","dlifeinteriors.com","weadesign.com","plus.unsplash.com","st.hzcdn.com"],
       },
 };
 
--- src/app/Components/Projects/projects.jsx
+++ src/app/Components/Projects/projects.jsx
@@ -4,7 +4,7 @@ const ProjectSection = () => {
   const projects = [
     { id: 1, src: 'https://dlifeinteriors.com/wp-content/uploads/2023/06/location-banglore-mobile-banner1.jpg', alt: 'Project 1', colSpan: 'lg:col-span-1', rowSpan: 'lg:row-span-2' },
     { id: 2, src: 'https://weadesign.com/wp-content/uploads/2024/03/luxury-bedroom-design-1200x900.webp', alt: 'Project 2', colSpan: 'lg:col-span-1', rowSpan: 'lg:row-span-1' },
-    { id: 3, src: 'https://www.floridaleathergallery.com/blog/wp-content/uploads/sites/116/2020/12/moderninteriordesign.jpg', alt: 'Project 3', colSpan: 'lg:col-span-2', rowSpan: 'lg:row-span-1' },
+    { id: 3, src: 'https://plus.unsplash.com/premium_photo-1670360414483-64e6d9ba9038?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW50ZXJpb3IlMjBkZXNpZ258ZW58MHx8MHx8fDA%3D', alt: 'Project 3', colSpan: 'lg:col-span-2', rowSpan: 'lg:row-span-1' },
     { id: 4, src: 'https://st.hzcdn.com/simgs/pictures/hallways/j-design-group-interior-designers-miami-bal-harbour-j-design-group-interior-designers-miami-modern-img~fa0187270eeb5d43_14-2889-1-0a6886b.jpg', alt: 'Project 4', colSpan: 'lg:col-span-1', rowSpan: 'lg:row-span-1' },
     { id: 5, src: 'https://plus.unsplash.com/premium_photo-1681113076872-c74b8926e70c?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8aW50ZXJpb3IlMjBkZXNpZ258ZW58MHx8MHx8fDA%3D', alt: 'Project 5', colSpan: 'lg:col-span-1', rowSpan: 'lg:row-span-1' },
     { id: 6, src: 'https://images.unsplash.com/photo-1586023492125-27b2c045efd7?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW50ZXJpb3IlMjBkZXNpZ258ZW58MHx8MHx8fDA%3D', alt: 'Project 6', colSpan: 'lg:col-span-1', rowSpan: 'lg:row-span-1' },
