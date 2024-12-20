280371a9ceac76c2ffb42383464c8cb8523d84c9
Vinayak Pandey <vinayakpandey@KTPLs-MacBook-Air.local>
2024-12-20 12:09:40 +0530

    desgin changes

--- src/app/loading.js
+++ src/app/loading.js
@@ -4,9 +4,6 @@ const Loading = () => {
   return (
     <div className="flex items-center justify-center h-[50vh] bg-gray-100">
       <div className="space-y-4 text-center">
-        {/* Spinner */}
-        <div className="w-16 h-16 border-4 border-blue-500 border-t-transparent rounded-full animate-spin mx-auto"></div>
-        {/* Loading Text */}
         <p className="text-lg font-medium text-gray-700 animate-pulse">
           Loading, please wait...
         </p>
